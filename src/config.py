9068e550bafa0746ff555e78ec02366a8ab56501
Yury Demidov <laikr@rambler.ru>
2025-08-11 19:31:10 +0300

    2025.08.11

--- src/config.py
+++ src/config.py
@@ -8,14 +8,13 @@ class Settings(BaseSettings):
     POSTGRES_USER: str = os.getenv('POSTGRES_USER')
     POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
     POSTGRES_DB: str = os.getenv('POSTGRES_DB')
-    DB_HOST: str = os.getenv('DB_HOST')
     DB_PORT: str = os.getenv('DB_PORT')
     SECRET_KEY: str = os.getenv('SECRET_KEY')
     ALGORITHM: str = os.getenv('ALGORITHM')
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
     DATABASE_URL: ClassVar[str] = (
         f'postgresql+psycopg2://{POSTGRES_USER}:'
-        f'{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}'
+        f'{POSTGRES_PASSWORD}@db:{DB_PORT}/{POSTGRES_DB}'
     )
 
     class Config:
